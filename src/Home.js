9a3498e788060832d570780a0506b4fc2f8f5465
Amritpal <amritpal15k@gmail.com>
2024-04-25 11:44:55 +0530

    lil update day 3

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,21 @@
-const Home = () => {
+import { useState } from 'react';
 
-    const handelClick=
+const Home = () => {
+    //let name='Ammy'
+    const [name , setName]=useState('Ammy');
+    const [age , setAge]=useState(25);
+
+    const handelClick=() =>{
+       setName('KAHLON');
+       setAge(30);
+    }
+    
     return (  
         <div className="home">
             <h2>Homepage</h2>
-            <button>ClickMe</button>
+            <p>{name} is {age} years old</p>
+            <button onClick={handelClick}>ClickMe</button>
+            
         </div>
     );
 }
