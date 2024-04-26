f9e640fdea998f2d9dcbc324d6de11ad5a763f9f
Amritpal <amritpal15k@gmail.com>
2024-04-26 18:43:50 +0530

    handeling fetch error

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,43 @@
-import { useState , useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs , setBlogs]=useState(null);
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error , setError]= useState(null);
 
-      const [name , setName]= useState('Ammy');
-      
-      useEffect(()=>{
-        fetch('http://localhost:8000/blogs')
-        .then(res =>{
+  const [name, setName] = useState('Ammy');
+
+  useEffect(() => {
+    setTimeout(() =>{
+      fetch('http://localhost:8000/blogs')
+        .then(res => {
+          if (!res.ok){
+            throw Error('Could not fetch the data fr the res')
+          }
           return res.json();
         })
-        .then(data =>{
-          
-          setBlogs(data);
+      .then(data => {
+        setBlogs(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch(err =>{
+        setIsPending(false);
+        setError(err.message);
+      })
+    }, 1000);
+  }, []);
 
-        })
-      } , []);
 
-    
-    return (  
-        <div className="home">         
-       {blogs && <BlogList blogs={blogs} title="ALL BLOGS"  /> }
-        
-        </div>
-    );
+  return (
+    <div className="home">
+      { error && <div> {error}</div>}
+      {isPending && <div> Loading... </div>}
+      {blogs && <BlogList blogs={blogs} title="ALL BLOGS" />}
+
+    </div>
+  );
 }
- 
+
 export default Home;
